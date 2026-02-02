f3f66668e8ee55f038cbbd65526726e4eb2514c3
thanongsaklaoketkich <thanong_777@hotmail.com>
2026-02-02 15:38:34 +0100

    Added application  and viteConfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ export default defineConfig({
   // This has to be equal to the name of your repository
   // For example, since this repository is https://github.com/kristiania-kws2100-2026/kws2100-kartbaserte-websystemer,
   //  `base` has to be `/kws2100-kartbaserte-websystemer`
-  base: "/kws2100-kartbaserte-websystemer",
+  base: "/kw2100-uke5",
 });
