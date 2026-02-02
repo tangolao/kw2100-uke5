fbf995a9137aa31e2f45b7fb697543fdf768cb4d
thanongsaklaoketkich <thanong_777@hotmail.com>
2026-02-02 15:27:46 +0100

    Added component

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // This has to be equal to the name of your repository
+  // For example, since this repository is https://github.com/kristiania-kws2100-2026/kws2100-kartbaserte-websystemer,
+  //  `base` has to be `/kws2100-kartbaserte-websystemer`
+  base: "/kws2100-kartbaserte-websystemer",
+});
