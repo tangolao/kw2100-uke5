866a26508e5ded5c1f5efc92228794aa83f9bf48
thanongsaklaoketkich <thanong_777@hotmail.com>
2026-02-04 06:06:08 +0100

    added fykeLayer

--- src/component/app/application.tsx
+++ src/component/app/application.tsx
@@ -11,6 +11,8 @@ import { useGeographic } from "ol/proj.js";
 // @ts-ignore
 import "ol/ol.css";
 import "./application.css";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
 
 // By calling the "useGeographic" function in OpenLayers, we tell that we want coordinates to be in degrees
 //  instead of meters, which is the default. Without this `center: [10.6, 59.9]` brings us to "null island"
@@ -18,12 +20,18 @@ useGeographic();
 
 // Here we create a Map object. Make sure you `import { Map } from "ol"`. Otherwise, the standard Javascript
 //  map data structure will be used
+const layers = [new TileLayer({ source: new OSM() })];
+const fylkeLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "http://",
+  }),
+});
 const map = new Map({
   // The map will be centered on a position in longitude (x-coordinate, east) and latitude (y-coordinate, north),
   //   with a certain zoom level
   view: new View({ center: [10.7, 59.9], zoom: 12 }),
+  layers,
   // map tile images will be from the Open Street Map (OSM) tile layer
-  layers: [new TileLayer({ source: new OSM() })],
 });
 
 // A functional React component
