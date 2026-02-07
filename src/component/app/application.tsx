85c0ccab54a2158c57521aff73b1c214f6857853
thanongsaklaoketkich <thanong_777@hotmail.com>
2026-02-07 04:41:59 +0100

    pointer move tracks active fylke

--- src/component/app/application.tsx
+++ src/component/app/application.tsx
@@ -61,7 +61,7 @@ export function Application() {
   return (
     <>
       <h1>
-        Fylke
+        Fylke:
         {activeFylke
           ? activeFylke.getProperties()["fylkesnavn"]
           : "Kart over administrative områder i Norge"}
